d44ae10c7c1ef5d7427099ba54cd034dcc829c13
김재훈 <choco730522@gmail.com>
2025-05-28 00:01:26 +0900

    Feat#15 (#24)
    * feat: 빌드 진행중
    
    * feat: 빌드 진행중
    
    * feat: 빌드 진행중

--- app.config.ts
+++ app.config.ts
@@ -6,8 +6,15 @@ export default {
         slug: 'cat-and-cash-front',
         version: '1.0.0',
         scheme: 'catandcash',
+        ios: {
+            bundleIdentifier: 'com.cau.catncash',
+            supportsTablet: true,
+          },
         extra: {
             API_BASE_URL: process.env.API_BASE_URL,
+            "eas": {
+                "projectId": "0f718091-7302-4a48-8691-e8aa706dbff9"
+            }
         },
     },
 };
